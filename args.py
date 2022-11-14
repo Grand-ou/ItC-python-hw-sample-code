93b5f384679fa71b5f05adf1a017f0604bf23c9f
Grand-ou <65006555+Grand-ou@users.noreply.github.com>
2022-11-14 08:30:05 +0800

    Update args.py
    args update

--- args.py
+++ args.py
@@ -6,5 +6,8 @@ def get_args():
     # TODO: Add --start-date, --end-date and --output arguments
     #       Convert the two dates to datetime objects
     parser = argparse.ArgumentParser()
+    parser.add_argument('--start-date', type=datetime, default=datetime.now(), help='initial start-date')
+    parser.add_argument('--end-date', type=datetime, default=datetime.now(), help='initial end-date')
+    parser.add_argument('--output', type=datetime, default=datetime.now(), help='initial output')
     args = parser.parse_args()
     return args
